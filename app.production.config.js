8d9d527b1f592d3d21ea24892808b656f274a506
ErikCupal <erik@cup.al>
2017-02-06 02:05:55 +0100

    fixed pm2 config

--- app.production.config.js
+++ app.production.config.js
@@ -4,7 +4,7 @@ module.exports = {
   apps: [
     {
       name: "erik-cupal-website-production",
-      script: "./build/production/app.js",
+      script: "./build/production/server.js",
       env: {
         PORT: 8081,
         NODE_ENV: "production"
