ad54bde723248d607a1570e505468f208d105f36
ErikCupal <erik@cup.al>
2017-02-06 01:42:13 +0100

    PM2 config created

---
+++ app.production.config.js
@@ -0,0 +1,14 @@
+// Production configuration file for PM2
+
+module.exports = {
+  apps: [
+    {
+      name: "erik-cupal-website-production",
+      script: "./build/production/app.js",
+      env: {
+        PORT: 8081,
+        NODE_ENV: "production"
+      }
+    }
+  ]
+}
